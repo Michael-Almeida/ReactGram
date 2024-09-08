bb49f14f8da8e3aa3ac88f02fc45a8401d40c480
Michael Cardoso <michaelcardoso97@gmail.com>
2024-09-08 15:58:14 -0300

    Criptografia com bcrypt

--- backEnd/controllers/UserController.js
+++ backEnd/controllers/UserController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -12,10 +12,43 @@ const generateToken = (id) => {
 };
 
 // Registrar usuário e logar
-const register = (req, res) => {
-  res.send("Register");
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Check se usuário existe
+  const user = await User.findOne({ email });
+
+  if (user) {
+    return res.status(422).json({ error: ["Por favor utilize outro e-mail."] });
+  }
+
+  // Generate password hash - TRANSFORMA A SENHA EM UM HASH (SEGURANÇA)
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  if (!newUser) {
+    return res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde"] });
+  }
+
+  res.status(201).json({
+    _id: newUser.id,
+    token: generateToken(newUser.id),
+  });
+};
+
+const login = (req, res) => {
+  res.send("Login");
 };
 
 module.exports = {
   register,
+  login,
 };
