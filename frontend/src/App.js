0c492ea4f085637fc64c6c58b80973133a9c3610
Michael Cardoso <michaelcardoso97@gmail.com>
2025-01-18 09:23:27 -0300

    Inicinando o front-end, criação de roteamento e limpeza de projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+
+import{BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+
+// Pages
+import Home from './components/Home/Home';
+import Login from './components/Auth/Login';
+import Register from "./components/Auth/Register"
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+     </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
