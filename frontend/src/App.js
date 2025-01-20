7038b09fb0588a0ecb8c194718c144108f4bdcb9
Michael Cardoso <michaelcardoso97@gmail.com>
2025-01-20 07:08:52 -0300

    criação de componentes navbar, footer, estilo css de ambas e geral

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
-
-import{BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // Pages
-import Home from './components/Home/Home';
-import Login from './components/Auth/Login';
-import Register from "./components/Auth/Register"
+import Home from "./components/Home/Home";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -1,8 +1,29 @@
+import { useState, useEffect } from "react";
 import "./Auth.css";
 
+import { Link } from "react-router-dom";
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
